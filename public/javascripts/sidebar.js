274172c5dce27f41cb59d9a1d2dde12feab10ca7
tujiaw <tujiaw@tujiawdeMacBook-Pro.local>
2017-02-02 14:32:12 +0800

    判断访问设备类型

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -13,5 +13,26 @@ $('.ui.search')
       url     : 'html_url'
     },
     minCharacters : 2
-  })
-;
+  });
+
+var isMobile = {
+  Android: function() {
+    return /Android/i.test(navigator.userAgent);
+  },
+  BlackBerry: function() {
+    return /BlackBerry/i.test(navigator.userAgent);
+  },
+  IOS: function() {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  },
+  Windows: function() {
+    return /IEMobile/i.test(navigator.userAgent);
+  },
+  any: function() {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+  }
+};
+
+if (isMobile.Android() || isMobile.IOS()) {
+  $('.ui.search').hide();
+}
